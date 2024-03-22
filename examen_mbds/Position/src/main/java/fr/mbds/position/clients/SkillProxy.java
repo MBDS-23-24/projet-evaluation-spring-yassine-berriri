a5ae5dbef05b4bbd88c641f8ce2b783bb438118a
yassineBr12 <yassine.berriri@etu.univ-cotedazur.fr>
2024-03-22 12:15:17 +0100

    add circuit-braker

--- examen_mbds/Position/src/main/java/fr/mbds/position/clients/SkillProxy.java
+++ examen_mbds/Position/src/main/java/fr/mbds/position/clients/SkillProxy.java
@@ -1,20 +1,28 @@
 package fr.mbds.position.clients;
 
 import fr.mbds.position.models.Skill;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "skill",url = "http://localhost:8888") // Remplacez SKILLSERVICE par le nom du service de compétences dans votre architecture
+//@FeignClient(name = "skill",url = "http://localhost:8888")
+@FeignClient(name = "SKILL")
 public interface SkillProxy {
 
     @GetMapping("/skills/{id}")
-
+    @CircuitBreaker(name = "skill"
+            , fallbackMethod =
+            "getDefaultSkill")
     Skill getSkill(@PathVariable("id") Long id); // L'annotation @PathVariable("id") s'assure que le paramètre de méthode est passé à l'URL
     @GetMapping("/skills")
 
+
+    @CircuitBreaker(name = "skill"
+            , fallbackMethod =
+            "getAllSkills")
     List<Skill> getSkills();
 
     // Méthode de secours pour getSkill en cas d'échec
