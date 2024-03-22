7dfbdd9272269e5edcd697fb7548499ba1270deb
yassineBr12 <yassine.berriri@etu.univ-cotedazur.fr>
2024-03-22 12:04:03 +0100

    fix gateway

--- examen_mbds/Position/src/main/java/fr/mbds/position/clients/SkillProxy.java
+++ examen_mbds/Position/src/main/java/fr/mbds/position/clients/SkillProxy.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "skill",url = "http://localhost:8092") // Remplacez SKILLSERVICE par le nom du service de compétences dans votre architecture
+@FeignClient(name = "skill",url = "http://localhost:8888") // Remplacez SKILLSERVICE par le nom du service de compétences dans votre architecture
 public interface SkillProxy {
 
     @GetMapping("/skills/{id}")
